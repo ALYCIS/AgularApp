17e0372f16050ee6443783f32c575d74369995b8
CISSE ALY* <alyhabcis@gmail.com>
2022-08-12 11:12:26 +0200

    Création des componants correspondant aux trois liens : sales, health et orders

--- mon-site-web/src/app/app-routing.module.ts
+++ mon-site-web/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'sidebar',component:SidebarComponent},
+  {path:'navbar',component:NavbarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
